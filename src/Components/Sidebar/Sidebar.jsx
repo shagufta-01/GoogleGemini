cc1c6121c18e084be8a188be34f70b7d67df8bc8
Shagufta fatima <129382422+shagufta-01@users.noreply.github.com>
2024-07-30 15:59:21 +0530

    first commit

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import './Sidebar.css'
+import { assets } from '../../../public/gemini-clone-assets/assets/assets'
+function Sidebar() {
+    const [extended, setExtended] =useState(false)
+  return (
+    <div className='sidebar'>
+      <div className='top'>
+<img onClick={()=>setExtended(prev=>!prev)}src={assets.menu_icon}className='menu' />
+<div className='new-chat'>
+    <img src ={assets.plus_icon}  />
+    {extended?<p>New Chat</p>:null}
+</div>
+{extended ?
+<div className='recent'>
+    <p className='recent-title'>Recent</p>
+    <div className='recent-entry'>
+    <img src={assets.message_icon}/>
+    <p>What is React...</p>
+    </div>
+
+</div>:null}
+      </div>
+      <div className='bottom'>
+<div className='botton-item recent-entry'>
+<img src={assets.question_icon}/>
+{extended?
+<p>Help</p>:null }
+</div>
+
+<div className='botton-item recent-entry'>
+<img src={assets.history_icon}/>{extended? <p>Activity</p>:null}
+</div>
+<div className='botton-item recent-entry'>
+<img src={assets.setting_icon}/>{extended?<p>Settings</p>:null}
+</div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
